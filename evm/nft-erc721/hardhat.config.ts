0c2d6cc78cafef2ae690071c012ddc568597ac27
Samyak Narnaware <110155540+Sammmmx@users.noreply.github.com>
2026-04-26 20:21:00 +0530

    Update hardhat.config.ts

--- evm/nft-erc721/hardhat.config.ts
+++ evm/nft-erc721/hardhat.config.ts
@@ -12,7 +12,7 @@ if (!isLocalNetwork) {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.27",
+    version: "0.8.26",
     settings: {
       evmVersion: "cancun",
       optimizer: {
